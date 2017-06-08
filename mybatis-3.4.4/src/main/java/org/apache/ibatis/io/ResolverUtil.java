4cb7968b0835e8e58ac00ffd79c7784396f1f54f
wusi06 <wusi0109@163.com>
2017-06-08 10:42:16 +0800

    添加类型注册器和util注释

--- mybatis-3.4.4/src/main/java/org/apache/ibatis/io/ResolverUtil.java
+++ mybatis-3.4.4/src/main/java/org/apache/ibatis/io/ResolverUtil.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2016 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2016 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.io;
 
@@ -25,6 +25,7 @@ import org.apache.ibatis.logging.Log;
 import org.apache.ibatis.logging.LogFactory;
 
 /**
+ *
  * <p>ResolverUtil is used to locate classes that are available in the/a class path and meet
  * arbitrary conditions. The two most common conditions are that a class implements/extends
  * another class, or that is it annotated with a specific annotation. However, through the use
@@ -57,212 +58,155 @@ import org.apache.ibatis.logging.LogFactory;
  * @author Tim Fennell
  */
 public class ResolverUtil<T> {
-  /*
-   * An instance of Log to use for logging in this class.
-   */
-  private static final Log log = LogFactory.getLog(ResolverUtil.class);
 
-  /**
-   * A simple interface that specifies how to test classes to determine if they
-   * are to be included in the results produced by the ResolverUtil.
-   */
-  public static interface Test {
+    private static final Log log = LogFactory.getLog(ResolverUtil.class);
+
     /**
-     * Will be called repeatedly with candidate classes. Must return True if a class
-     * is to be included in the results, false otherwise.
+     * 函数式接口，判断是否符合
      */
-    boolean matches(Class<?> type);
-  }
-
-  /**
-   * A Test that checks to see if each class is assignable to the provided class. Note
-   * that this test will match the parent type itself if it is presented for matching.
-   */
-  public static class IsA implements Test {
-    private Class<?> parent;
-
-    /** Constructs an IsA test using the supplied Class as the parent class/interface. */
-    public IsA(Class<?> parentType) {
-      this.parent = parentType;
+    public static interface Test {
+        boolean matches(Class<?> type);
     }
 
-    /** Returns true if type is assignable to the parent type supplied in the constructor. */
-    @Override
-    public boolean matches(Class<?> type) {
-      return type != null && parent.isAssignableFrom(type);
-    }
+    /**
+     * 函数式接口的具体实现类
+     */
+    public static class IsA implements Test {
+        //通过构造函数注入
+        private Class<?> parent;
 
-    @Override
-    public String toString() {
-      return "is assignable to " + parent.getSimpleName();
-    }
-  }
-
-  /**
-   * A Test that checks to see if each class is annotated with a specific annotation. If it
-   * is, then the test returns true, otherwise false.
-   */
-  public static class AnnotatedWith implements Test {
-    private Class<? extends Annotation> annotation;
-
-    /** Constructs an AnnotatedWith test for the specified annotation type. */
-    public AnnotatedWith(Class<? extends Annotation> annotation) {
-      this.annotation = annotation;
-    }
-
-    /** Returns true if the type is annotated with the class provided to the constructor. */
-    @Override
-    public boolean matches(Class<?> type) {
-      return type != null && type.isAnnotationPresent(annotation);
-    }
-
-    @Override
-    public String toString() {
-      return "annotated with @" + annotation.getSimpleName();
-    }
-  }
-
-  /** The set of matches being accumulated. */
-  private Set<Class<? extends T>> matches = new HashSet<Class<? extends T>>();
-
-  /**
-   * The ClassLoader to use when looking for classes. If null then the ClassLoader returned
-   * by Thread.currentThread().getContextClassLoader() will be used.
-   */
-  private ClassLoader classloader;
-
-  /**
-   * Provides access to the classes discovered so far. If no calls have been made to
-   * any of the {@code find()} methods, this set will be empty.
-   *
-   * @return the set of classes that have been discovered.
-   */
-  public Set<Class<? extends T>> getClasses() {
-    return matches;
-  }
-
-  /**
-   * Returns the classloader that will be used for scanning for classes. If no explicit
-   * ClassLoader has been set by the calling, the context class loader will be used.
-   *
-   * @return the ClassLoader that will be used to scan for classes
-   */
-  public ClassLoader getClassLoader() {
-    return classloader == null ? Thread.currentThread().getContextClassLoader() : classloader;
-  }
-
-  /**
-   * Sets an explicit ClassLoader that should be used when scanning for classes. If none
-   * is set then the context classloader will be used.
-   *
-   * @param classloader a ClassLoader to use when scanning for classes
-   */
-  public void setClassLoader(ClassLoader classloader) {
-    this.classloader = classloader;
-  }
-
-  /**
-   * Attempts to discover classes that are assignable to the type provided. In the case
-   * that an interface is provided this method will collect implementations. In the case
-   * of a non-interface class, subclasses will be collected.  Accumulated classes can be
-   * accessed by calling {@link #getClasses()}.
-   *
-   * @param parent the class of interface to find subclasses or implementations of
-   * @param packageNames one or more package names to scan (including subpackages) for classes
-   */
-  public ResolverUtil<T> findImplementations(Class<?> parent, String... packageNames) {
-    if (packageNames == null) {
-      return this;
-    }
-
-    Test test = new IsA(parent);
-    for (String pkg : packageNames) {
-      find(test, pkg);
-    }
-
-    return this;
-  }
-
-  /**
-   * Attempts to discover classes that are annotated with the annotation. Accumulated
-   * classes can be accessed by calling {@link #getClasses()}.
-   *
-   * @param annotation the annotation that should be present on matching classes
-   * @param packageNames one or more package names to scan (including subpackages) for classes
-   */
-  public ResolverUtil<T> findAnnotated(Class<? extends Annotation> annotation, String... packageNames) {
-    if (packageNames == null) {
-      return this;
-    }
-
-    Test test = new AnnotatedWith(annotation);
-    for (String pkg : packageNames) {
-      find(test, pkg);
-    }
-
-    return this;
-  }
-
-  /**
-   * Scans for classes starting at the package provided and descending into subpackages.
-   * Each class is offered up to the Test as it is discovered, and if the Test returns
-   * true the class is retained.  Accumulated classes can be fetched by calling
-   * {@link #getClasses()}.
-   *
-   * @param test an instance of {@link Test} that will be used to filter classes
-   * @param packageName the name of the package from which to start scanning for
-   *        classes, e.g. {@code net.sourceforge.stripes}
-   */
-  public ResolverUtil<T> find(Test test, String packageName) {
-    String path = getPackagePath(packageName);
-
-    try {
-      List<String> children = VFS.getInstance().list(path);
-      for (String child : children) {
-        if (child.endsWith(".class")) {
-          addIfMatching(test, child);
+        public IsA(Class<?> parentType) {
+            this.parent = parentType;
         }
-      }
-    } catch (IOException ioe) {
-      log.error("Could not read package: " + packageName, ioe);
+
+        @Override
+        public boolean matches(Class<?> type) {
+            //这个判断parent是否是type的超类或同类
+            return type != null && parent.isAssignableFrom(type);
+        }
+
+        @Override
+        public String toString() {
+            return "is assignable to " + parent.getSimpleName();
+        }
     }
 
-    return this;
-  }
+    /**
+     * 函数式接口的具体实现类
+     * 判断class是否添加了指定注解，注解通过构造函数注入，如果添加了返回true，否则返回false
+     */
+    public static class AnnotatedWith implements Test {
+        private Class<? extends Annotation> annotation;
 
-  /**
-   * Converts a Java package name to a path that can be looked up with a call to
-   * {@link ClassLoader#getResources(String)}.
-   * 
-   * @param packageName The Java package name to convert to a path
-   */
-  protected String getPackagePath(String packageName) {
-    return packageName == null ? null : packageName.replace('.', '/');
-  }
-
-  /**
-   * Add the class designated by the fully qualified class name provided to the set of
-   * resolved classes if and only if it is approved by the Test supplied.
-   *
-   * @param test the test used to determine if the class matches
-   * @param fqn the fully qualified name of a class
-   */
-  @SuppressWarnings("unchecked")
-  protected void addIfMatching(Test test, String fqn) {
-    try {
-      String externalName = fqn.substring(0, fqn.indexOf('.')).replace('/', '.');
-      ClassLoader loader = getClassLoader();
-      if (log.isDebugEnabled()) {
-        log.debug("Checking to see if class " + externalName + " matches criteria [" + test + "]");
-      }
-
-      Class<?> type = loader.loadClass(externalName);
-      if (test.matches(type)) {
-        matches.add((Class<T>) type);
-      }
-    } catch (Throwable t) {
-      log.warn("Could not examine class '" + fqn + "'" + " due to a " +
-          t.getClass().getName() + " with message: " + t.getMessage());
+        //构造器注入要判断的指定注解
+        public AnnotatedWith(Class<? extends Annotation> annotation) {
+            this.annotation = annotation;
+        }
+        @Override
+        public boolean matches(Class<?> type) {
+            return type != null && type.isAnnotationPresent(annotation);
+        }
+        @Override
+        public String toString() {
+            return "annotated with @" + annotation.getSimpleName();
+        }
     }
-  }
+
+    //匹配类型容器
+    private Set<Class<? extends T>> matches = new HashSet<Class<? extends T>>();
+
+    //查找class的类加载器，没有就用Thread.currentThread().getContextClassLoader()
+    private ClassLoader classloader;
+
+    //符合的类型集合，执行find()方法 没有的话返回空
+    public Set<Class<? extends T>> getClasses() {
+        return matches;
+    }
+
+    public ClassLoader getClassLoader() {
+        return classloader == null ? Thread.currentThread().getContextClassLoader() : classloader;
+    }
+
+    public void setClassLoader(ClassLoader classloader) {
+        this.classloader = classloader;
+    }
+
+   //查找package下面的Parent的同类或者实现类
+    public ResolverUtil<T> findImplementations(Class<?> parent, String... packageNames) {
+        if (packageNames == null) {
+            return this;
+        }
+        //实例化对象 IsA并且注入parent类，返回ResolverUtil，查看集合matches可以查看到符合的类型
+        Test test = new IsA(parent);
+        for (String pkg : packageNames) {
+            find(test, pkg);
+        }
+        return this;
+    }
+
+    //查找package下面的annotation注解的类
+    public ResolverUtil<T> findAnnotated(Class<? extends Annotation> annotation, String... packageNames) {
+        if (packageNames == null) {
+            return this;
+        }
+        Test test = new AnnotatedWith(annotation);
+        for (String pkg : packageNames) {
+            find(test, pkg);
+        }
+        return this;
+    }
+
+    //传入test函数表达式，传入packageName添加符合的类
+    public ResolverUtil<T> find(Test test, String packageName) {
+        //获取可以查询的path, ClassLoader#getResources(String)要用
+        String path = getPackagePath(packageName);
+
+        try {
+            List<String> children = VFS.getInstance().list(path);
+            for (String child : children) {
+                if (child.endsWith(".class")) {
+                    addIfMatching(test, child);
+                }
+            }
+        } catch (IOException ioe) {
+            log.error("Could not read package: " + packageName, ioe);
+        }
+        return this;
+    }
+
+    /**
+     * Converts a Java package name to a path that can be looked up with a call to
+     * {@link ClassLoader#getResources(String)}.
+     *
+     * @param packageName The Java package name to convert to a path
+     */
+    protected String getPackagePath(String packageName) {
+        return packageName == null ? null : packageName.replace('.', '/');
+    }
+
+    /**
+     * 这个地方用了策略模式的设计方法，传入的只是Test接口，不知道具体的实现类，类似于lambda表达式
+     *
+     * @param test the test used to determine if the class matches
+     * @param fqn the fully qualified name of a class(查询出来的)
+     */
+    @SuppressWarnings("unchecked")
+    protected void addIfMatching(Test test, String fqn) {
+        try {
+            // fqn为sample/mybatis/domain/City.class去掉class，并且"/"变成"."，通过这个名字取得到Class类型
+            String externalName = fqn.substring(0, fqn.indexOf('.')).replace('/', '.');
+            ClassLoader loader = getClassLoader();
+            if (log.isDebugEnabled()) {
+                log.debug("Checking to see if class " + externalName + " matches criteria [" + test + "]");
+            }
+            Class<?> type = loader.loadClass(externalName);
+            //如果符合的话，就将class对象添加到matches集合中，这个地方调用test的match方法，如果符合就添加
+            if (test.matches(type)) {
+                matches.add((Class<T>) type);
+            }
+        } catch (Throwable t) {
+            log.warn("Could not examine class '" + fqn + "'" + " due to a " +
+                    t.getClass().getName() + " with message: " + t.getMessage());
+        }
+    }
 }
--- mybatis-3.4.4/src/main/java/org/apache/ibatis/type/TypeAliasRegistry.java
+++ mybatis-3.4.4/src/main/java/org/apache/ibatis/type/TypeAliasRegistry.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2015 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.type;
 
@@ -39,154 +39,158 @@ import org.apache.ibatis.io.Resources;
  */
 public class TypeAliasRegistry {
 
-  //别名注册到Map中，key为名字， value为class类型， final
-  private final Map<String, Class<?>> TYPE_ALIASES = new HashMap<String, Class<?>>();
+    //别名注册到Map中，key为名字， value为class类型， final
+    private final Map<String, Class<?>> TYPE_ALIASES = new HashMap<String, Class<?>>();
 
-  public TypeAliasRegistry() {
-    //初始化别名，包括了java基本类型
-    registerAlias("string", String.class);
+    public TypeAliasRegistry() {
+        //初始化别名，包括了java基本类型
+        registerAlias("string", String.class);
 
-    registerAlias("byte", Byte.class);
-    registerAlias("long", Long.class);
-    registerAlias("short", Short.class);
-    registerAlias("int", Integer.class);
-    registerAlias("integer", Integer.class);
-    registerAlias("double", Double.class);
-    registerAlias("float", Float.class);
-    registerAlias("boolean", Boolean.class);
+        registerAlias("byte", Byte.class);
+        registerAlias("long", Long.class);
+        registerAlias("short", Short.class);
+        registerAlias("int", Integer.class);
+        registerAlias("integer", Integer.class);
+        registerAlias("double", Double.class);
+        registerAlias("float", Float.class);
+        registerAlias("boolean", Boolean.class);
 
-    //初始化别名，包括了java基本类型数组
-    registerAlias("byte[]", Byte[].class);
-    registerAlias("long[]", Long[].class);
-    registerAlias("short[]", Short[].class);
-    registerAlias("int[]", Integer[].class);
-    registerAlias("integer[]", Integer[].class);
-    registerAlias("double[]", Double[].class);
-    registerAlias("float[]", Float[].class);
-    registerAlias("boolean[]", Boolean[].class);
+        //初始化别名，包括了java基本类型数组
+        registerAlias("byte[]", Byte[].class);
+        registerAlias("long[]", Long[].class);
+        registerAlias("short[]", Short[].class);
+        registerAlias("int[]", Integer[].class);
+        registerAlias("integer[]", Integer[].class);
+        registerAlias("double[]", Double[].class);
+        registerAlias("float[]", Float[].class);
+        registerAlias("boolean[]", Boolean[].class);
 
-    //初始化别名，包括了java基本类型数组，非包装类型，名字前面加下划线
-    registerAlias("_byte", byte.class);
-    registerAlias("_long", long.class);
-    registerAlias("_short", short.class);
-    registerAlias("_int", int.class);
-    registerAlias("_integer", int.class);
-    registerAlias("_double", double.class);
-    registerAlias("_float", float.class);
-    registerAlias("_boolean", boolean.class);
+        //初始化别名，包括了java基本类型数组，非包装类型，名字前面加下划线
+        registerAlias("_byte", byte.class);
+        registerAlias("_long", long.class);
+        registerAlias("_short", short.class);
+        registerAlias("_int", int.class);
+        registerAlias("_integer", int.class);
+        registerAlias("_double", double.class);
+        registerAlias("_float", float.class);
+        registerAlias("_boolean", boolean.class);
 
-    registerAlias("_byte[]", byte[].class);
-    registerAlias("_long[]", long[].class);
-    registerAlias("_short[]", short[].class);
-    registerAlias("_int[]", int[].class);
-    registerAlias("_integer[]", int[].class);
-    registerAlias("_double[]", double[].class);
-    registerAlias("_float[]", float[].class);
-    registerAlias("_boolean[]", boolean[].class);
+        registerAlias("_byte[]", byte[].class);
+        registerAlias("_long[]", long[].class);
+        registerAlias("_short[]", short[].class);
+        registerAlias("_int[]", int[].class);
+        registerAlias("_integer[]", int[].class);
+        registerAlias("_double[]", double[].class);
+        registerAlias("_float[]", float[].class);
+        registerAlias("_boolean[]", boolean[].class);
 
-    registerAlias("date", Date.class);
-    registerAlias("decimal", BigDecimal.class);
-    registerAlias("bigdecimal", BigDecimal.class);
-    registerAlias("biginteger", BigInteger.class);
-    registerAlias("object", Object.class);
+        registerAlias("date", Date.class);
+        registerAlias("decimal", BigDecimal.class);
+        registerAlias("bigdecimal", BigDecimal.class);
+        registerAlias("biginteger", BigInteger.class);
+        registerAlias("object", Object.class);
 
-    registerAlias("date[]", Date[].class);
-    registerAlias("decimal[]", BigDecimal[].class);
-    registerAlias("bigdecimal[]", BigDecimal[].class);
-    registerAlias("biginteger[]", BigInteger[].class);
-    registerAlias("object[]", Object[].class);
+        registerAlias("date[]", Date[].class);
+        registerAlias("decimal[]", BigDecimal[].class);
+        registerAlias("bigdecimal[]", BigDecimal[].class);
+        registerAlias("biginteger[]", BigInteger[].class);
+        registerAlias("object[]", Object[].class);
 
-    registerAlias("map", Map.class);
-    registerAlias("hashmap", HashMap.class);
-    registerAlias("list", List.class);
-    registerAlias("arraylist", ArrayList.class);
-    registerAlias("collection", Collection.class);
-    registerAlias("iterator", Iterator.class);
+        registerAlias("map", Map.class);
+        registerAlias("hashmap", HashMap.class);
+        registerAlias("list", List.class);
+        registerAlias("arraylist", ArrayList.class);
+        registerAlias("collection", Collection.class);
+        registerAlias("iterator", Iterator.class);
 
-    //这个感觉会出问题
-    registerAlias("ResultSet", ResultSet.class);
-  }
-
-  @SuppressWarnings("unchecked")
-  // 抛出异常 if types cannot be assigned
-  public <T> Class<T> resolveAlias(String string) {
-    try {
-      if (string == null) {
-        return null;
-      }
-      // 转换成小写 issue #748
-      String key = string.toLowerCase(Locale.ENGLISH);
-      Class<T> value;
-      //如果缓存中包含直接取出返回，如果没有就调用Resources的classForName()，如果转换失败，抛出异常ClassNotFoundException
-      if (TYPE_ALIASES.containsKey(key)) {
-        value = (Class<T>) TYPE_ALIASES.get(key);
-      } else {
-        value = (Class<T>) Resources.classForName(string);
-      }
-      return value;
-    } catch (ClassNotFoundException e) {
-      throw new TypeException("Could not resolve type alias '" + string + "'.  Cause: " + e, e);
+        //这个感觉会出问题
+        registerAlias("ResultSet", ResultSet.class);
     }
-  }
 
-  public void registerAliases(String packageName){
-    //注册为Object类型
-    registerAliases(packageName, Object.class);
-  }
-
-  public void registerAliases(String packageName, Class<?> superType){
-    ResolverUtil<Class<?>> resolverUtil = new ResolverUtil<Class<?>>();
-    resolverUtil.find(new ResolverUtil.IsA(superType), packageName);
-    Set<Class<? extends Class<?>>> typeSet = resolverUtil.getClasses();
-    for(Class<?> type : typeSet){
-      // 忽略内部类和接口（including package-info.java) See issue #6
-      if (!type.isAnonymousClass() && !type.isInterface() && !type.isMemberClass()) {
-        registerAlias(type);
-      }
+    @SuppressWarnings("unchecked")
+    // 抛出异常 if types cannot be assigned
+    public <T> Class<T> resolveAlias(String string) {
+        try {
+            if (string == null) {
+                return null;
+            }
+            // 转换成小写 issue #748
+            String key = string.toLowerCase(Locale.ENGLISH);
+            Class<T> value;
+            //如果缓存中包含直接取出返回，如果没有就调用Resources的classForName()，如果转换失败，抛出异常ClassNotFoundException
+            if (TYPE_ALIASES.containsKey(key)) {
+                value = (Class<T>) TYPE_ALIASES.get(key);
+            } else {
+                value = (Class<T>) Resources.classForName(string);
+            }
+            return value;
+        } catch (ClassNotFoundException e) {
+            throw new TypeException("Could not resolve type alias '" + string + "'.  Cause: " + e, e);
+        }
     }
-  }
 
-  //注册添加了注解@Alias 的类型到缓存
-  public void registerAlias(Class<?> type) {
-    //默认simpleName
-    String alias = type.getSimpleName();
-    Alias aliasAnnotation = type.getAnnotation(Alias.class);
-    //如果注解有value，就用value
-    if (aliasAnnotation != null) {
-      alias = aliasAnnotation.value();
-    } 
-    registerAlias(alias, type);
-  }
+    public void registerAliases(String packageName) {
+        //注册为Object类型
+        registerAliases(packageName, Object.class);
+    }
 
-  public void registerAlias(String alias, Class<?> value) {
-    if (alias == null) {
-      throw new TypeException("The parameter alias cannot be null");
-    }
-    // 别名都是小写的
-    String key = alias.toLowerCase(Locale.ENGLISH);
-    //如果已经包含key 且 通过key取得的value不为空 且 通过key取得的value不等于传入的value
-    if (TYPE_ALIASES.containsKey(key) && TYPE_ALIASES.get(key) != null && !TYPE_ALIASES.get(key).equals(value)) {
-      throw new TypeException("The alias '" + alias + "' is already mapped to the value '" + TYPE_ALIASES.get(key).getName() + "'.");
-    }
-    //别名放入到map中
-    TYPE_ALIASES.put(key, value);
-  }
+    public void registerAliases(String packageName, Class<?> superType) {
 
-  public void registerAlias(String alias, String value) {
-    try {
-      registerAlias(alias, Resources.classForName(value));
-    } catch (ClassNotFoundException e) {
-      throw new TypeException("Error registering type alias "+alias+" for "+value+". Cause: " + e, e);
+        ResolverUtil<Class<?>> resolverUtil = new ResolverUtil<Class<?>>();
+        //寻找packageName下面的，父类为superType的所有子类
+        resolverUtil.find(new ResolverUtil.IsA(superType), packageName);
+        //调用getClasses获取符合类型集合
+        Set<Class<? extends Class<?>>> typeSet = resolverUtil.getClasses();
+        for (Class<?> type : typeSet) {
+            // 忽略内部类和接口（including package-info.java) See issue #6
+            if (!type.isAnonymousClass() && !type.isInterface() && !type.isMemberClass()) {
+                registerAlias(type);
+            }
+        }
     }
-  }
-  
-  /**
-   * 获取不能修改的别名类型集合
-   *
-   * @since 3.2.2
-   */
-  public Map<String, Class<?>> getTypeAliases() {
-    return Collections.unmodifiableMap(TYPE_ALIASES);
-  }
+
+    //注册添加了注解@Alias 的类型到缓存
+    public void registerAlias(Class<?> type) {
+        //默认simpleName
+        String alias = type.getSimpleName();
+        Alias aliasAnnotation = type.getAnnotation(Alias.class);
+        //如果注解有value，就用value
+        if (aliasAnnotation != null) {
+            alias = aliasAnnotation.value();
+        }
+        registerAlias(alias, type);
+    }
+
+    //注册别名
+    public void registerAlias(String alias, Class<?> value) {
+        if (alias == null) {
+            throw new TypeException("The parameter alias cannot be null");
+        }
+        // 别名都是小写的
+        String key = alias.toLowerCase(Locale.ENGLISH);
+        //如果已经包含key 且 通过key取得的value不为空 且 通过key取得的value不等于传入的value
+        if (TYPE_ALIASES.containsKey(key) && TYPE_ALIASES.get(key) != null && !TYPE_ALIASES.get(key).equals(value)) {
+            throw new TypeException("The alias '" + alias + "' is already mapped to the value '" + TYPE_ALIASES.get(key).getName() + "'.");
+        }
+        //别名放入到map中
+        TYPE_ALIASES.put(key, value);
+    }
+
+    public void registerAlias(String alias, String value) {
+        try {
+            registerAlias(alias, Resources.classForName(value));
+        } catch (ClassNotFoundException e) {
+            throw new TypeException("Error registering type alias " + alias + " for " + value + ". Cause: " + e, e);
+        }
+    }
+
+    /**
+     * 获取不能修改的别名类型集合
+     *
+     * @since 3.2.2
+     */
+    public Map<String, Class<?>> getTypeAliases() {
+        return Collections.unmodifiableMap(TYPE_ALIASES);
+    }
 
 }
