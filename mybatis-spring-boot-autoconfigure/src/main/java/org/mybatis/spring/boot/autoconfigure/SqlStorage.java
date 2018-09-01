57e9eedfaef77a86f56c2d0132165e0e385771d8
wusi <wusi@dianwoba.com>
2018-09-01 17:04:44 +0800

    添加 批量操作xml demo

---
+++ mybatis-spring-boot-autoconfigure/src/main/java/org/mybatis/spring/boot/autoconfigure/SqlStorage.java
@@ -0,0 +1,45 @@
+package org.mybatis.spring.boot.autoconfigure;
+
+import org.apache.ibatis.mapping.MappedStatement;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.CollectionUtils;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.*;
+
+
+/**
+ * @author wusi
+ * @version 2018/8/28.
+ */
+@Configuration
+@ConditionalOnBean(SqlSessionTemplate.class)
+public class SqlStorage {
+
+    @Resource
+    private SqlSessionTemplate sessionTemplate;
+
+    @PostConstruct
+    public void writeSql() {
+        String FILE_NAME = "sql.txt";
+        Collection<MappedStatement> mappedStatements = sessionTemplate.getConfiguration().getMappedStatements();
+        Set<MappedStatement> mappedStatementsSet = new HashSet<>(mappedStatements);
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(FILE_NAME));
+            if (sessionTemplate != null && !CollectionUtils.isEmpty(mappedStatementsSet)) {
+                for (MappedStatement mappedStatement : mappedStatementsSet) {
+                    out.write(mappedStatement.getSqlSource().getBoundSql(null).getSql());
+                    out.write("\r\n");
+                }
+                out.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
